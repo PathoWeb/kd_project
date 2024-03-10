790b94dd8a303632dca33cf905e4339e32271d6e
unknown <amirghhastam@gmail.com>
2024-03-10 22:17:34 +0330

    Add react router dom and write footer lin

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // start Footer css
 import './Footer.css'
+import { Link } from 'react-router-dom'
 
 export default function Footer () {
   return (
@@ -11,11 +12,10 @@ export default function Footer () {
         {/* start dark blue circle */}
         <div className=' h-full w-full bg-[#343f5f] rounded-tr-[20%] rounded-tl-[130%]'>
           {/*start  black circle */}
-          <div className='h-full w-full bg-[#000000]  rounded-tl-[500%]'>
+          <div className='h-full w-full bg-[#181810]  rounded-tl-[500%]'>
             {/* start footer introduction and access and map */}
             <div className='footer-content'>
               <div className='footer-content-intro'>
-                <img src='' alt='KD-logo' className='footer-intro-img' />
                 <p className='footer-intro-text'>
                   لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
                   استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و
@@ -25,14 +25,51 @@ export default function Footer () {
                   آینده شناخت فراوان جامعه و متخصصان را می طلبد
                 </p>
               </div>
-              <div className='footer-content-access'></div>
-              <div className='footer-content-map'></div>
+              <div className='footer-content-access'>
+                <h5>دسترسی سریع</h5>
+                <ul className='footer-access-lists'>
+                  <li className='footer-access-link'>
+                    <Link to='/' className='access-link'>
+                      صفحه اصلی
+                    </Link>
+                  </li>
+                  <li className='footer-access-link'>
+                    <Link to='/' className='access-link'>
+                      مقالات
+                    </Link>
+                  </li>
+                  <li className='footer-access-link'>
+                    <Link to='/' className='access-link'>
+                      فروشگاه
+                    </Link>
+                  </li>
+                  <li className='footer-access-link'>
+                    <Link to='/' className='access-link'>
+                      آموزش
+                    </Link>
+                  </li>
+                  <li className='footer-access-link'>
+                    <Link to='/' className='access-link'>
+                      درباره ما
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className='footer-content-map rounded-full border-solid border-2 border-gray-500 w-80 h-80'></div>
             </div>
             {/* start footer address and phone number and media link */}
             <div className='footer-info'>
-              <div className='footer-info-content'></div>
-              <div className='footer-info-content'></div>
-              <div className='footer-info-content'></div>
+              <div className='footer-info-content'>
+                <p className='footer-address'></p>
+              </div>
+              <div className='footer-info-content'>
+                <p className='footer-phone'></p>
+              </div>
+              <div className='footer-info-content'>
+                <span className='footer-social'></span>
+                <span className='footer-social'></span>
+                <span className='footer-social'></span>
+              </div>
             </div>
             {/* start footer copy-right */}
             <div className='footer-copy-right'></div>
