6e21784c8d2228d0de74749c966a4e3a48a3e423
unknown <amirghhastam@gmail.com>
2024-03-16 15:55:20 +0330

    Write the title and rectangle on the back of the page

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Services.css'
+import SectionTitle from '../SectionTitle/SectionTitle'
 export default function Services () {
   return (
-    <div>
-      <h2>hello world</h2>
+    <div className='services my-10'>
+      <SectionTitle title='خدمات آکادمی' />
+
+      <div className='bg-rectangle w-full h-[250px] bg-[#343f5f] rounded-3xl'></div>
     </div>
   )
 }
