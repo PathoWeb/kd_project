5fb5228bdf29d2eb9b8264cff615d0423b49ae75
unknown <amirghhastam@gmail.com>
2024-03-16 15:16:33 +0330

    Start Services section

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Services.css'
+export default function Services () {
+  return (
+    <div>
+      <h2>hello world</h2>
+    </div>
+  )
+}
--- src/Pages/IndexPage/IndexPage.jsx
+++ src/Pages/IndexPage/IndexPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './IndexPage.css'
 import Footer from '../../Components/Footer/Footer'
+import Services from '../../Components/Services/Services'
 
 export default function IndexPage () {
   return (
     <div className=''>
+      <Services />
       <Footer />
     </div>
   )
